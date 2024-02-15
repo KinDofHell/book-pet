5260a4a3343338648f78012c34cf9c07e1d0973a
KinDofHell <ihordzhus24@gmail.com>
2024-02-15 12:19:52 +0200

    category model

---
+++ lib/database/index.ts
@@ -0,0 +1,21 @@
+import * as mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDB = async () => {
+  if (cached.conn) return cached.conn;
+  if (!MONGO_URI) throw new Error("MONGO_URI is missing");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGO_URI, {
+      dbName: "bookGlossary",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
