9c3538502cf4a9f33fd82bb6bbc668bb5d5ac70f
kindofhell <ihordzhus24@gmail.com>
2024-03-09 15:59:26 +0200

    created schema for glossary item

---
+++ lib/database/models/glossaryItem.model.ts
@@ -0,0 +1,35 @@
+import { model, models, Schema } from "mongoose";
+import { ObjectType } from "@clerk/backend";
+
+export interface IGlossaryItem extends Document {
+  _id: string;
+  title: string;
+  description: string;
+  history: string;
+  additional?: string;
+  imageUrl: string;
+  categoryId: string;
+  isVisible: boolean;
+  tableInfo: [{ key: string; value: string }];
+}
+
+const TableInfoSchema = new Schema({
+  key: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const GlossaryItemSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  history: { type: String, required: true },
+  additional: { type: String },
+  imageUrl: { type: String, required: true },
+  categoryId: { type: Schema.Types.ObjectId, ref: "Category" },
+  isVisible: { type: Boolean, default: false },
+  tableInfo: [TableInfoSchema],
+});
+
+const GlossaryItem =
+  models.GlossaryItem || model("GlossaryItem", GlossaryItemSchema);
+
+export default GlossaryItem;
